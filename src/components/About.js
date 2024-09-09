7a625d3bd1eca8562da66eb5955e775bcf46d549
Ningrui Zhang <ningrui1227@gmail.com>
2024-09-09 12:48:30 +0100

    update business-card

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
     return (
         <div className="about--container">
             <h2 className="about--title">About</h2>
-            <p className="about--text">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+            <p className="about--text">I am a frontend developer with skills in JavaScript, React, Python, C, Github and Firebase. Exploring web development with a creative mindset and gaining hands-on experience in building user-friendly applications.</p>
         </div>
     )
 }
---
+++ src/components/Portfolio.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Portfolio() {
+    return (
+        <div className="portfolio--container">
+            <h2 className="portfolio--title">Portfolio</h2>
+            <a href="https://shimmering-malasada-04a43c.netlify.app" target="_blank" rel="noopener noreferrer" className="portfolio--link">
+                React Notes app
+            </a>
+        </div>
+    )
+}
