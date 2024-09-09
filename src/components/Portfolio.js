1247dd967437c34baa202c687a70c160a234e25f
Ningrui Zhang <ningrui1227@gmail.com>
2024-09-09 20:36:23 +0100

    add github souce code button and modify css style for it

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,9 +4,16 @@ export default function Portfolio() {
     return (
         <div className="portfolio--container">
             <h2 className="portfolio--title">Portfolio</h2>
-            <a href="https://shimmering-malasada-04a43c.netlify.app" target="_blank" rel="noopener noreferrer" className="portfolio--link">
-                React Notes app
-            </a>
+            <div className="portfolio--link-container">
+                <a href="https://shimmering-malasada-04a43c.netlify.app" target="_blank" rel="noopener noreferrer" className="portfolio--link">
+                    React Notes App
+                </a>
+                <button>
+                    <a href="https://github.com/nngrz/React-project-set/tree/main/notes_app" target="_blank" rel="noopener noreferrer" className="portfolio--btn">
+                        Source Code
+                    </a>
+                </button>
+            </div>
         </div>
     )
 }
