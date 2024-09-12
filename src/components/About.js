a9742653811dd30eaa15cc573178472bd2c8585d
Ningrui Zhang <ningrui1227@gmail.com>
2024-09-12 21:11:33 +0100

    update content

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
     return (
         <div className="about--container">
             <h2 className="about--title">About</h2>
-            <p className="about--text">I am a frontend developer with skills in JavaScript, React, Python, C, Github and Firebase. Exploring web development with a creative mindset and gaining hands-on experience in building user-friendly applications.</p>
+            <p className="about--text">I am a frontend developer with skills in JavaScript, React, Python, C, Git, GitHub, GitHub workflows and Firebase. Exploring web development with a creative mindset and gaining hands-on experience in building user-friendly applications.</p>
         </div>
     )
 }
