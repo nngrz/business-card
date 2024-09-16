037bef24ed1238db9930d83cd7532f177739426a
Ningrui Zhang <ningrui1227@gmail.com>
2024-09-16 16:58:01 +0100

    in portfolio section list technologies that were used and adjust the style

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,14 +5,30 @@ export default function Portfolio() {
         <div className="portfolio--container">
             <h2 className="portfolio--title">Portfolio</h2>
             <div className="portfolio--link-container">
-                <a href="https://shimmering-malasada-04a43c.netlify.app" target="_blank" rel="noopener noreferrer" className="portfolio--link">
-                    React Notes App
-                </a>
-                <button>
-                    <a href="https://github.com/nngrz/React-project-set/tree/main/notes_app" target="_blank" rel="noopener noreferrer" className="portfolio--btn">
-                        Source Code
+                <div className="projects">
+                    {/* project--React Notes App */}
+                    <a href="https://shimmering-malasada-04a43c.netlify.app" target="_blank" rel="noopener noreferrer" className="project--link">
+                        React Notes App
                     </a>
-                </button>
+                    <button>
+                        <a href="https://github.com/nngrz/React-project-set/tree/main/notes_app" target="_blank" rel="noopener noreferrer" className="project--btn">
+                            Source Code
+                        </a>
+                    </button>
+                    <div className="project--technologies-bar">
+                            <div className="bar js-react-notes"></div>
+                            <div className="bar css-react-notes"></div>
+                            <div className="bar html-react-notes"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="technologies--legend">
+                <div class="legend-item">
+                    <span class="legend-color js"></span> JavaScript/React </div>
+                <div class="legend-item">
+                    <span class="legend-color css"></span> CSS </div>
+                <div class="legend-item">
+                    <span class="legend-color html"></span> HTML </div>
             </div>
         </div>
     )
