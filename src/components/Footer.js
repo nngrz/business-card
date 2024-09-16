cd85550fb04011490a493f4c54f27506d2fe6fdd
Ningrui Zhang <ningrui1227@gmail.com>
2024-09-16 17:49:46 +0100

    adjust footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,6 @@ import React from "react"
 
 export default function Footer() {
     return (
-        <nav className="footer--container">
-            
-        </nav>
+        <nav className="footer--container"></nav>
     )
 }
