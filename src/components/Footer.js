167116cb15c967cfb15ea59d77e6bf77d71f8443
Ningrui Zhang <ningrui1227@gmail.com>
2024-09-16 17:31:53 +0100

    remove not working icons in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,8 @@ import React from "react"
 
 export default function Footer() {
     return (
-        <nav className="icons">
-            <img src="images/twitter.png"></img>
-            <img src="images/facebook.png"></img>
-            <img src="images/ig.png"></img>
-            <a href="https://github.com/nngrz" target="_blank" rel="noopener noreferrer">
-                <img src="images/github.png" alt="GitHub Profile" className="icon-github" />
-            </a>
+        <nav className="footer--container">
+            
         </nav>
     )
 }
