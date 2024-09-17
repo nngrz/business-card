4a2561dd1bec02c63fa274afd9469750b96ff4d9
Ningrui Zhang <ningrui1227@gmail.com>
2024-09-17 15:43:24 +0100

    adjust feature of download cv button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Footer() {
     return (
         <nav className="footer--container">
-            <a href="NingruiZhang_CV_Sample.pdf" download="NingruiZhang_CV_Sample.pdf" className="cv--link">
+            <a href="NingruiZhang_CV_Sample.pdf" target="_blank" rel="noopener noreferrer" className="cv--link">
                 DOWNLOAD CV
             </a>
         </nav>
